153d29d1e006d26f1f5d9037494a6ec6c57b1b6c
Khushboo <khushboo1028@gmail.com>
2020-11-10 00:13:36 +0530

    Home Page Done

---
+++ src/components/HeroSection/index.js
@@ -0,0 +1,41 @@
+import React, { useState } from 'react';
+import {
+	HeroContainer,
+	HeroBg,
+	VideoBg,
+	HeroContent,
+	HeroH1,
+	HeroP,
+	ArrowForward,
+	ArrowRight,
+	HeroBtnWrapper
+} from './HeroElements';
+import Video from '../../videos/video.mp4';
+import { Button } from '../ButtonElement';
+
+const HeroSection = () => {
+	const [ hover, setHover ] = useState(false);
+
+	const onHover = () => {
+		setHover(!hover);
+	};
+
+	return (
+		<HeroContainer>
+			<HeroBg>
+				<VideoBg autoPlay loop muted src={Video} type="video/mp4" />
+			</HeroBg>
+			<HeroContent>
+				<HeroH1>Happy Birthday Poppyyyy</HeroH1>
+				<HeroP> Welcome to your own personal page!</HeroP>
+				<HeroBtnWrapper>
+					<Button primary="true" dark="true" onMouseEnter={onHover} onMouseLeave={onHover} to="signup">
+						Get Started {hover ? <ArrowForward /> : <ArrowRight />}
+					</Button>
+				</HeroBtnWrapper>
+			</HeroContent>
+		</HeroContainer>
+	);
+};
+
+export default HeroSection;
